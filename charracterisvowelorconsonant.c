90822639a6b8f8ba4060df811299f1b77e577fcf
adithak <adithak59@gmail.com>
2017-09-28 11:55:15 +0530

    Create charracterisvowelorconsonant.c

---
+++ charracterisvowelorconsonant.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char m;
+int vowel1,vowel2;
+printf("enter the charracter");
+scanf("%c",&c);
+vowel1=(m=='a' || m=='e' || m=='i' || m=='o' || m=='u' );
+vowel2=(m=='A' || m=='E' || m=='I' || m=='O' || m=='U');
+if(vowel1 || vowel2);
+printf("%c is a vowel",c);
+else
+printf("%c is a consonant",c);
+return 0;
+}
